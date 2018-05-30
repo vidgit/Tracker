c6e584320261a010ee576258ca89f2364633d604
vidgit <vibhorjoshi05@gmail.com>
2018-05-30 14:46:51 +0530

    Fetch completed

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,34 @@
+document.getElementById('issueInputForm').addEventListener('submit',saveIssue);
+
+function saveIssue(e){
+    
+}
+
 function fetchIssues(){
     var issues = JSON.parse(localStorage.getItem("issues"));
     if(issues === null){
-        Console.log("No Issues to load.");
+        console.log("No Issues to load.");
+    }
+    else{
+        var issuesList = document.getElementById("issues");
+        issuesList.innerHTML = "";
+
+        for( var i = 0; i<issues.length; i++){
+            var id= issues[i].id;
+            var desc = issues[i].description;
+            var severity =  issues.severity;
+            var assignee = issues[i].assignedTo;
+            var status = issues[i].status;
+
+            issuesList.innerHTML += '<div class="well" >'+
+                                    '<h6>Issue ID:'+id+' </h6>'+
+                                    '<p><span class="label label-info">'+status+'</span></p>'+
+                                    '<h3>'+desc+'</h3>'+
+                                    '<p><span class="glyphicon glyphicon-time"></span>' + severity +"</p>"+
+                                    '<p><span class="glyphicon glyphicon-user"></span>' + assignedTo + "</p>"+
+                                    '<a href="#" onclick="setStatusClosed(\''+id+'\')" class="btn btn-warning">Close</a>'+
+                                    '<a href="# onclick="deleteIssue(\'' + id +'\')" class="btn btn-danger"> Delete </a>'+
+                                    '</div>';
+            }
     }
 }
